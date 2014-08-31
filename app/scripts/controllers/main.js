70fe9b9f116949e6b563cd190bcd9f20519be1b2
anderskristo <anderskristo@gmail.com>
2014-08-31 21:17:35 +0200

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('guityApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
